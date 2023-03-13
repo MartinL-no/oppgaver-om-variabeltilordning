c8846ac104ce83ee4871a78b5e110811cac09c0a
martinlrmr <102468994+MartinL-no@users.noreply.github.com>
2023-03-13 13:34:28 +0100

    oppgave-1

---
+++ oppgaver-om-variabeltilordning/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace oppgaver_om_variabeltilordning
+{
+    class Program
+    {
+        int OppgaveEnInt = 1;
+        long OppgaveEnLong = 123456789;
+        float OppgaveEnFloat = 1.01f;
+        decimal OppgaveEnDecimal = 1.000001m;
+        double OppgaveEnDouble = 12333323.45;
+        string OppgaveEnString = "a string";
+        char OppgaveEnChar = 'a';
+        bool OppgaveEnBool = true;
+
+        static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+            }
+    }
+}
+
